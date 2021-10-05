8a095154604ea1bcac3c86e3a36d2bb78a03acdf
pc <pc@DESKTOP-BI5FP4B>
2021-10-05 13:47:13 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ console.log("Shoppinglist")
 
 let myElement = document.getElementById("0");
 console.log(myElement);
+//myElement.setAttribute("class","healthy");
+//myElement.setAttribute("hidden","true");
 
 let myList = document.getElementsByTagName("li");
+myList.setAttribute("class","healthy");
+
 console.log(myList);
