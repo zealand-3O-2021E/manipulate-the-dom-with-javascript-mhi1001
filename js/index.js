d683f511372675023d793265b4bb21b47cf9f9be
pc <pc@DESKTOP-BI5FP4B>
2021-10-05 13:34:35 +0200

    Added some js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+let myElement = document.getElementById("0");
+console.log(myElement);
+
+let myList = document.getElementsByTagName("li");
+console.log(myList);
